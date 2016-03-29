964fdc012a89fa0f2970f70e384aa32978d51c7c
Pi-Hsun Shih <peter50216@gmail.com>
2016-03-29 10:49:10 +0800

    Reorder assert_equal so expected is first.
    Also, 100% coverage :P

--- test/context_test.rb
+++ test/context_test.rb
@@ -6,48 +6,48 @@ class ContextTest < MiniTest::Test
 
   def test_update
     context.update(arch: 'arm', os: 'windows')
-    assert_equal(context.arch, 'arm')
-    assert_equal(context.os, 'windows')
+    assert_equal('arm', context.arch)
+    assert_equal('windows', context.os)
   end
 
   def test_local
     context.timeout = 1
-    assert_equal(context.timeout, 1)
+    assert_equal(1, context.timeout)
 
     context.local(timeout: 2) do
-      assert_equal(context.timeout, 2)
+      assert_equal(2, context.timeout)
       context.timeout = 3
-      assert_equal(context.timeout, 3)
+      assert_equal(3, context.timeout)
     end
 
-    assert_equal(context.timeout, 1)
+    assert_equal(1, context.timeout)
   end
 
   def test_clear
     default_arch = context.arch
     context.arch = 'arm'
     context.clear
-    assert_equal(context.arch, default_arch)
+    assert_equal(default_arch, context.arch)
   end
 
   def test_arch
     context.arch = 'mips'
-    assert_equal(context.arch, 'mips')
+    assert_equal('mips', context.arch)
 
     err = assert_raises(ArgumentError) { context.arch = 'shik' }
     assert_match(/arch must be one of/, err.message)
-    assert_equal(context.arch, 'mips')
+    assert_equal('mips', context.arch)
 
     context.clear
-    assert_equal(context.bits, 32)
+    assert_equal(32, context.bits)
     context.arch = 'powerpc64'
-    assert_equal(context.bits, 64)
-    assert_equal(context.endian, 'big')
+    assert_equal(64, context.bits)
+    assert_equal('big', context.endian)
   end
 
   def test_bits
     context.bits = 64
-    assert_equal(context.bits, 64)
+    assert_equal(64, context.bits)
 
     err = assert_raises(ArgumentError) { context.bits = 0 }
     assert_match(/bits must be > 0/, err.message)
@@ -55,19 +55,19 @@ class ContextTest < MiniTest::Test
 
   def test_bytes
     context.bytes = 8
-    assert_equal(context.bits, 64)
-    assert_equal(context.bytes, 8)
+    assert_equal(64, context.bits)
+    assert_equal(8, context.bytes)
 
     context.bits = 32
-    assert_equal(context.bytes, 4)
+    assert_equal(4, context.bytes)
   end
 
   def test_endian
     context.endian = 'le'
-    assert_equal(context.endian, 'little')
+    assert_equal('little', context.endian)
 
     context.endian = 'big'
-    assert_equal(context.endian, 'big')
+    assert_equal('big', context.endian)
 
     err = assert_raises(ArgumentError) { context.endian = 'SUPERBIG' }
     assert_match(/endian must be one of/, err.message)
@@ -75,10 +75,10 @@ class ContextTest < MiniTest::Test
 
   def test_log_level
     context.log_level = 'error'
-    assert_equal(context.log_level, Logger::ERROR)
+    assert_equal(Logger::ERROR, context.log_level)
 
     context.log_level = 514
-    assert_equal(context.log_level, 514)
+    assert_equal(514, context.log_level)
 
     err = assert_raises(ArgumentError) { context.log_level = 'BOOM' }
     assert_match(/log_level must be an integer or one of/, err.message)
@@ -86,7 +86,7 @@ class ContextTest < MiniTest::Test
 
   def test_os
     context.os = 'windows'
-    assert_equal(context.os, 'windows')
+    assert_equal('windows', context.os)
 
     err = assert_raises(ArgumentError) { context.os = 'deepblue' }
     assert_match(/os must be one of/, err.message)
@@ -94,10 +94,10 @@ class ContextTest < MiniTest::Test
 
   def test_signed
     context.signed = true
-    assert_equal(context.signed, true)
+    assert_equal(true, context.signed)
 
     context.signed = 'unsigned'
-    assert_equal(context.signed, false)
+    assert_equal(false, context.signed)
 
     err = assert_raises(ArgumentError) { context.signed = 'partial' }
     assert_match(/signed must be boolean or one of/, err.message)
@@ -105,11 +105,15 @@ class ContextTest < MiniTest::Test
 
   def test_timeout
     context.timeout = 123
-    assert_equal(context.timeout, 123)
+    assert_equal(123, context.timeout)
   end
 
   def test_newline
     context.newline = "\r\n"
-    assert_equal(context.newline, "\r\n")
+    assert_equal("\r\n", context.newline)
+  end
+
+  def test_to_s
+    assert_match(/\APwnlib::Context::ContextType\(.+\)\Z/, context.to_s)
   end
 end
--- test/util/packing_test.rb
+++ test/util/packing_test.rb
@@ -6,44 +6,44 @@ class PackingTest < MiniTest::Test
   include Pwnlib::Util::Packing
 
   def test_pack
-    assert_equal(pack(0x414243, 24, 'big', true), 'ABC')
-    assert_equal(pack(0x414243, 24, 'little', true), 'CBA')
+    assert_equal('ABC', pack(0x414243, 24, 'big', true))
+    assert_equal('CBA', pack(0x414243, 24, 'little', true))
 
-    assert_equal(pack(0x814243, 24, 'big', false), "\x81BC")
+    assert_equal("\x81BC", pack(0x814243, 24, 'big', false))
     err = assert_raises(ArgumentError) { pack(0x814243, 23, 'big', false) }
     assert_match(/does not fit/, err.message)
 
-    assert_equal(pack(0x814243, 25, 'big', true), "\x00\x81BC")
+    assert_equal("\x00\x81BC", pack(0x814243, 25, 'big', true))
     err = assert_raises(ArgumentError) { pack(0x814243, 24, 'big', true) }
     assert_match(/does not fit/, err.message)
 
-    assert_equal(pack(-1, 'all', 'little', true), "\xff")
-    assert_equal(pack(-256, 'all', 'big', true), "\xff\x00")
-    assert_equal(pack(0xdeadbeef, 'all', 'big', false), "\xde\xad\xbe\xef")
-    assert_equal(pack(0x0102030405, 'all', 'little', true), "\x05\x04\x03\x02\x01")
-    assert_equal(pack(0x80000000, 'all', 'little', true), "\x00\x00\x00\x80\x00")
+    assert_equal("\xff", pack(-1, 'all', 'little', true))
+    assert_equal("\xff\x00", pack(-256, 'all', 'big', true))
+    assert_equal("\xde\xad\xbe\xef", pack(0xdeadbeef, 'all', 'big', false))
+    assert_equal("\x05\x04\x03\x02\x01", pack(0x0102030405, 'all', 'little', true))
+    assert_equal("\x00\x00\x00\x80\x00", pack(0x80000000, 'all', 'little', true))
 
     err = assert_raises(ArgumentError) { pack('shik') }
     assert_match(/must be an integer/, err.message)
 
-    assert_equal(pack(0x414243, bits: 24, endian: 'big'), 'ABC')
+    assert_equal('ABC', pack(0x414243, bits: 24, endian: 'big'))
 
     err = assert_raises(ArgumentError) { pack(-514, bits: 'all', signed: 'unsigned') }
     assert_match(/Can't pack negative number/, err.message)
   end
 
   def test_unpack
-    assert_equal(unpack("\xaa\x55", 16, 'little', false), 0x55aa)
-    assert_equal(unpack("\xaa\x55", 16, 'big', false), 0xaa55)
-    assert_equal(unpack("\xaa\x55", 16, 'big', true), -0x55ab)
-    assert_equal(unpack("\xaa\x55", 15, 'big', true), 0x2a55)
-    assert_equal(unpack("\xff\x02\x03", 'all', 'little', true), 0x0302ff)
-    assert_equal(unpack("\xff\x02\x03", 'all', 'big', true), -0xfdfd)
-    assert_equal(unpack("\x00\x00\x00\x80\x00", 'all', 'little', true), 0x80000000)
+    assert_equal(0x55aa, unpack("\xaa\x55", 16, 'little', false))
+    assert_equal(0xaa55, unpack("\xaa\x55", 16, 'big', false))
+    assert_equal(-0x55ab, unpack("\xaa\x55", 16, 'big', true))
+    assert_equal(0x2a55, unpack("\xaa\x55", 15, 'big', true))
+    assert_equal(0x0302ff, unpack("\xff\x02\x03", 'all', 'little', true))
+    assert_equal(-0xfdfd, unpack("\xff\x02\x03", 'all', 'big', true))
+    assert_equal(0x80000000, unpack("\x00\x00\x00\x80\x00", 'all', 'little', true))
 
     err = assert_raises(ArgumentError) { unpack("\xff\xff", 8, 'big', false) }
     assert_match(/does not match/, err.message)
 
-    assert_equal(unpack('ABC', bits: 'all', endian: 'big', signed: false), 0x414243)
+    assert_equal(0x414243, unpack('ABC', bits: 'all', endian: 'big', signed: false))
   end
 end
