55221082d9937e4e3d3bf9ce5463c4ab1382d228
Pi-Hsun Shih <peter50216@gmail.com>
2016-04-08 15:14:40 +0800

    Add one more test to ensure(?) context isn't everywhere.

--- test/files/use_pwnlib.rb
+++ test/files/use_pwnlib.rb
@@ -1,7 +1,15 @@
 # encoding: ASCII-8BIT
+# TODO(Darkpi): Should we make sure ALL module works? (maybe we should).
 require 'pwnlib/util/packing'
 
 raise 'call from module fail' unless Pwnlib::Util::Packing.p8(0x61) == 'a'
 
 include Pwnlib::Util::Packing::ClassMethod
 raise 'include module and call fail' unless p8(0x61) == 'a'
+
+begin
+  Pwnlib::Util::Packing.context
+  raise 'context public in Pwnlib module'
+rescue NoMethodError
+  puts 'good'
+end
