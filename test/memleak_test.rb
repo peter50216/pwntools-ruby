78ceabd96d0e60674414e439d3200326fbc14548
hanhanW <hanhan0912@gmail.com>
2016-06-22 16:23:25 +0800

    Use popen2 instead popen3

--- test/memleak_test.rb
+++ test/memleak_test.rb
@@ -13,7 +13,7 @@ class MemLeakTest < MiniTest::Test
     assert_equal(0, @leak.find_elf_base(@binsh.length * 2 / 3))
     [32, 64].each do |b|
       # TODO(hh): Use process instead of popen3
-      Open3.popen3(File.expand_path("../data/victim#{b}", __FILE__)) do |i, o, _e, t|
+      Open3.popen2(File.expand_path("../data/victim#{b}", __FILE__)) do |i, o, t|
         main_ra = o.readline[2...-1].to_i(16)
         realbase = nil
         IO.readlines("/proc/#{t.pid}/maps").map(&:split).each do |s|
