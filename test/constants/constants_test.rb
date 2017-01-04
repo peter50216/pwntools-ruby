93df83bced07d887236129e255d7d6402042503c
david942j <david942j@gmail.com>
2017-01-05 01:51:24 +0800

    add i386

--- test/constants/constants_test.rb
+++ test/constants/constants_test.rb
@@ -13,4 +13,12 @@ class ConstantsTest < MiniTest::Test
       # assert_equal('Constant("O_CREAT | O_WRONLY", 0x41)', ::Pwnlib::Constants.eval('O_CREAT | O_WRONLY').inspect)
     end
   end
+
+  def test_i386
+    context.local(arch: 'i386') do
+      assert_equal('Constant("SYS_read", 0x3)', send(:SYS_read).inspect)
+      assert_equal('__NR_prctl', send(:__NR_prctl).to_s)
+      assert_equal('Constant("O_CREAT", 0x40)', ::Pwnlib::Constants.eval('O_CREAT').inspect)
+    end
+  end
 end
