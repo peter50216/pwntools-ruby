388d8543f74ca5aa0d53b58da87dcaf51df6f9ce
david942j <david942j@gmail.com>
2017-01-05 14:20:51 +0800

    add some methods to Constant

--- lib/pwnlib/constants/constant.rb
+++ lib/pwnlib/constants/constant.rb
@@ -3,10 +3,37 @@
 module Pwnlib
   module Constants
     # A class that includes name and value
+    #
+    # @note This class `looks like` inherit from {Fixnum}. While {Fixnum} is not inheritable, so there're some hacking methods.
     class Constant
+      attr_reader :str, :val
       def initialize(str, val)
         @str = str
         @val = val
+      end
+
+      # To looks like inherit from {Fixnum}
+      def method_missing(method, *args, &block)
+        @val.send(method, *args, &block)
+      end
+
+      def is_a?(klass)
+        return true if @val.is_a? klass
+        super
+      end
+
+      # @example
+      #   Constant.new('SYS_read', 0) == 0 #=> true
+      #   Constant.new('SYS_read', 0) == Constant.new('SYS_read', 0) #=> true
+      #   Constant.new('SYS_read', 0) == Constant.new('__NR_read', 0) #=> false
+      def ==(other)
+        return @str == other.str && @val == other.val if other.is_a? Constant
+        return @val == other if other.is_a? Fixnum
+        super
+      end
+
+      def to_i
+        @val
       end
 
       def to_s
--- test/constants/constants_test.rb
+++ test/constants/constants_test.rb
@@ -1,9 +1,25 @@
 # encoding: ASCII-8BIT
 require 'test_helper'
 require 'pwnlib/constants/constants'
+require 'pwnlib/constants/constant'
 
 class ConstantsTest < MiniTest::Test
   include Pwnlib::Constants::ClassMethod
+  Constant = ::Pwnlib::Constants::Constant
+
+  def test_constant_methods
+    a1 = Constant.new('a', 1)
+    assert_equal(9, Constant.new('c', 9).to_i)
+    assert_equal(3, a1 | 2)
+    assert_operator(Constant.new('ZERO', 0), :==, 0)
+    assert_operator(a1, :==, Constant.new('a', 1))
+    assert_equal(false, a1 == Constant.new('b', 1))
+    assert_equal(false, a1 == 'a')
+    assert_equal(true, a1.is_a?(Fixnum))
+    assert_equal(true, a1.is_a?(Constant))
+    assert_equal(false, a1.is_a?(String))
+  end
+
   def test_amd64
     context.local(arch: 'amd64') do
       assert_equal('Constant("SYS_read", 0)', send(:SYS_read).inspect)
