1af5a995c2dcfc22d5ec4eb325c64cfdc467cff1
david942j <david942j@gmail.com>
2017-01-06 17:20:09 +0800

    let AsmRenderer be a module

--- lib/pwnlib/shellcraft/shellcraft.rb
+++ lib/pwnlib/shellcraft/shellcraft.rb
@@ -9,12 +9,13 @@ module Pwnlib
   module Shellcraft
     def self.method_missing(method, *args, &_)
       # no asm need block?
-      AsmRender.run(method, args) || Submodule.get(method) || super
+      AsmRenderer.run(method, args) || Submodule.get(method) || super
     end
 
     # To support like Shellcraft.amd64.linux.syscall.
     #
-    # return a Submodule object when call Shellcraft.amd64, and do the `directory traversal'.
+    # return a Submodule object when call Shellcraft.amd64, and do the
+    # `directories traversal' for furthur calling.
     class Submodule
       def initialize(name)
         @name = name
@@ -23,7 +24,7 @@ module Pwnlib
       def method_missing(method, *args, &_)
         super unless exists?(method)
         return Submodule.get(File.join(@name, method.to_s)) if dir?(method)
-        AsmRender.run(method, args, path: @name) || super
+        AsmRenderer.run(method, args, path: @name) || super
       end
 
       private
@@ -37,75 +38,19 @@ module Pwnlib
       end
 
       def path(method)
-        File.join(AsmRender::TEMPLATES, @name, method.to_s)
+        File.join(AsmRenderer::TEMPLATES, @name, method.to_s)
       end
 
       def self.get(name)
         name = name.to_s
-        return nil unless File.exist?(File.join(AsmRender::TEMPLATES, name))
+        return nil unless File.exist?(File.join(AsmRenderer::TEMPLATES, name))
         Submodule.new(name)
       end
     end
 
-    # Class for running .rb to acquire the result assembly.
-    class AsmRender
-      include ::Pwnlib
+    # For render templates.
+    module AsmRenderer
       TEMPLATES = File.join(__dir__, 'templates')
-      def initialize(method, filename, args)
-        @method = method
-        @filename = filename
-        @args = args
-      end
-
-      # Pass to module Shellcraft when method missing.
-      def method_missing(method, *args, &block)
-        Shellcraft.send(method, *args, &block)
-      end
-
-      def work
-        @_output = ''
-        instance_eval(File.read(@filename))
-        # method {@method} must be defined after instance_eval.
-        send(@method, *@args)
-        typesetting
-      end
-
-      private
-
-      # Indent each line 2 space.
-      # TODO(david942j): consider labels
-      def typesetting
-        @_output.lines.map do |line|
-          ' ' * 2 + line.lstrip
-        end.join
-      end
-
-      # For templates/*.rb use
-
-      def cat(str)
-        @_output.concat str + (str.end_with?("\n") ? '' : "\n")
-      end
-
-      def okay(s, *a, **kw)
-        s = Util::Packing.pack(s, *a, **kw) if s.is_a?(Integer)
-        !(s.include?("\x00") || s.include?("\n"))
-      end
-
-      def eval(item)
-        return item if item.is_a?(Integer)
-        Constants.eval(item)
-      end
-      alias evaluate eval
-
-      def pretty(n, comment: true)
-        return n.inspect if n.is_a?(String)
-        return n unless n.is_a?(Numeric)
-        if n.instance_of?(Constants::Constant)
-          return format(comment ? '%s /* %s */' : '%s (%s)', n, pretty(n.to_i))
-        end
-        return n if n.abs < 10
-        Util::Fiddling.hex(n)
-      end
       # Static methods.
       class << self
         # Check if can find {name}.rb in current context.
@@ -129,14 +74,76 @@ module Pwnlib
         #   If `nil` is given, path will be treated as "./#{context.arch}/**/"
         #
         # @example
-        #   AsmRender.run(:mov, ['rax', 'rbx'], path: './amd64/')
+        #   AsmRenderer.run(:mov, ['rax', 'rbx'], path: './amd64/')
         #   => " mov rax, rbx\n"
         def run(method, args, path: nil)
           path ||= "./#{context.arch}/**/"
           return nil unless exists?(path, method)
-          AsmRender.new(method, file_of(path, method), args).work
+          typesetting(Render.new(method, file_of(path, method), args).work)
         end
+
+        # Indent each line 2 space.
+        # TODO(david942j): consider labels
+        def typesetting(result)
+          result.lines.map do |line|
+            ' ' * 2 + line.lstrip
+          end.join
+        end
+
         include ::Pwnlib::Context
+      end
+
+      # Class for providing a `sandbox' to run *.rb and acquire the result assembly.
+      class Render
+        # Then we don't need Pwnlib:: in templates.
+        include ::Pwnlib
+        def initialize(method, filename, args)
+          @method = method
+          @filename = filename
+          @args = args
+        end
+
+        # Pass to module Shellcraft when method missing.
+        def method_missing(method, *args, &block)
+          Shellcraft.send(method, *args, &block)
+        end
+
+        def work
+          @_output = ''
+          instance_eval(File.read(@filename))
+          # method {@method} must be defined after instance_eval.
+          send(@method, *@args)
+          @_output
+        end
+
+        private
+
+        # For templates/*.rb use.
+
+        def cat(str)
+          @_output.concat str + (str.end_with?("\n") ? '' : "\n")
+        end
+
+        def okay(s, *a, **kw)
+          s = Util::Packing.pack(s, *a, **kw) if s.is_a?(Integer)
+          !(s.include?("\x00") || s.include?("\n"))
+        end
+
+        def eval(item)
+          return item if item.is_a?(Integer)
+          Constants.eval(item)
+        end
+        alias evaluate eval
+
+        def pretty(n, comment: true)
+          return n.inspect if n.is_a?(String)
+          return n unless n.is_a?(Numeric)
+          if n.instance_of?(Constants::Constant)
+            return format(comment ? '%s /* %s */' : '%s (%s)', n, pretty(n.to_i))
+          end
+          return n if n.abs < 10
+          Util::Fiddling.hex(n)
+        end
       end
     end
   end
