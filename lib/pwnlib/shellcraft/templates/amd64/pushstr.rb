fb775920819835daf12f9ba992a75c60ffd0528a
david942j <david942j@gmail.com>
2017-01-13 02:36:01 +0800

    amd64.pushstr

---
+++ lib/pwnlib/shellcraft/templates/amd64/pushstr.rb
@@ -0,0 +1,45 @@
+require 'pwnlib/util/fiddling'
+require 'pwnlib/util/lists'
+require 'pwnlib/util/packing'
+require 'pwnlib/shellcraft/shellcraft'
+
+# Push a string to stack
+::Pwnlib::Shellcraft.define('amd64.pushstr') do |str, append_null: true|
+  extend ::Pwnlib::Util::Fiddling::ClassMethods
+  extend ::Pwnlib::Util::Lists::ClassMethods
+  extend ::Pwnlib::Util::Packing::ClassMethods
+  # This will not effect callee's +str+.
+  str += "\x00" if append_null && !str.end_with?("\x00")
+  next if str.empty?
+  padding = str[-1].ord >= 128 ? "\xff" : "\x00"
+  cat "/* push #{str.inspect} */"
+  group(8, str, underfull_action: :fill, fill_value: padding).reverse.each do |word|
+    sign = u64(word, endian: 'little', signed: true)
+    sign32 = u32(word[0, 4], bits: 32, endian: 'little', signed: true)
+    # simple forbidden byte case
+    if [0, 0xa].include?(sign)
+      cat "push #{pretty(sign + 1)}"
+      cat 'dec byte ptr [rsp]'
+    # simple byte case
+    elsif sign >= -0x80 && sign <= 0x7f
+      cat "push #{pretty(sign)}"
+    # simple 32bit without forbidden byte
+    elsif sign >= -0x80000000 && sign <= 0x7fffffff && okay(word[0, 4])
+      cat "push #{pretty(sign)}"
+    elsif okay(word)
+      cat "mov rax, #{pretty(sign)}"
+      cat 'push rax'
+    # The high 4 byte of word are all zeros, so we can use +xor dword ptr [rsp]+.
+    elsif sign32 == sign
+      a = u32(xor_pair(word[0, 4]).first, endian: 'little', signed: true)
+      cat "push #{pretty(a)} ^ #{pretty(sign)}"
+      cat "xor dword ptr [rsp], #{pretty(a)}"
+    else
+      a = u64(xor_pair(word).first, endian: 'little', signed: false)
+      cat "mov rax, #{pretty(a)}"
+      cat 'push rax'
+      cat "mov rax, #{pretty(a)} ^ #{pretty(sign)}"
+      cat 'xor [rsp], rax'
+    end
+  end
+end
---
+++ test/shellcraft/pushstr_test.rb
@@ -0,0 +1,54 @@
+# encoding: ASCII-8BIT
+require 'test_helper'
+require 'pwnlib/shellcraft/shellcraft'
+require 'pwnlib/context'
+
+class PushstrTest < MiniTest::Test
+  include ::Pwnlib::Context
+
+  def setup
+    @shellcraft = ::Pwnlib::Shellcraft.instance
+  end
+
+  def test_amd64
+    context.local(arch: 'amd64') do
+      assert_equal(<<-'EOS', @shellcraft.pushstr('A'))
+  /* push "A\x00" */
+  push 0x41
+      EOS
+      assert_equal(<<-'EOS', @shellcraft.pushstr("\n"))
+  /* push "\n\x00" */
+  push 0xb
+  dec byte ptr [rsp]
+      EOS
+      assert_equal(<<-'EOS', @shellcraft.pushstr('A' * 4))
+  /* push "AAAA\x00" */
+  push 0x41414141
+      EOS
+      assert_equal(<<-'EOS', @shellcraft.pushstr('A' * 8))
+  /* push "AAAAAAAA\x00" */
+  push 1
+  dec byte ptr [rsp]
+  mov rax, 0x4141414141414141
+  push rax
+      EOS
+      assert_equal(<<-'EOS', @shellcraft.pushstr('A' * 8, append_null: false))
+  /* push "AAAAAAAA" */
+  mov rax, 0x4141414141414141
+  push rax
+      EOS
+      assert_equal(<<-'EOS', @shellcraft.pushstr("\n" * 4))
+  /* push "\n\n\n\n\x00" */
+  push 0x1010101 ^ 0xa0a0a0a
+  xor dword ptr [rsp], 0x1010101
+      EOS
+      assert_equal(<<-'EOS', @shellcraft.pushstr('/bin/sh'))
+  /* push "/bin/sh\x00" */
+  mov rax, 0x101010101010101
+  push rax
+  mov rax, 0x101010101010101 ^ 0x68732f6e69622f
+  xor [rsp], rax
+      EOS
+    end
+  end
+end
