c45c224b5264e30f3a0a2ebb2d3511f93e810603
david942j <david942j@gmail.com>
2017-02-16 01:46:48 +0800

    merge pushstr_array

--- lib/pwnlib/abi.rb
+++ lib/pwnlib/abi.rb
@@ -10,10 +10,13 @@ module Pwnlib
       attr_reader :register_arguments
       # Only use for x86, to specific the +eax+, +edx+ pair.
       attr_reader :cdq_pair
-      def initialize(regs, align, minimum, cdq_pair: nil)
+      attr_reader :arg_alignment
+      attr_reader :stack_register
+      def initialize(regs, align, minimum, stack_register, cdq_pair: nil)
         @register_arguments = regs
         @arg_alignment = align
         @stack_minimum = minimum
+        @stack_register = stack_register
         @cdq_pair = cdq_pair
       end
 
@@ -41,18 +44,16 @@ module Pwnlib
     # which must be loaded into the specified register.
     class SyscallABI < ABI
       attr_reader :syscall_str
-      attr_reader :stack_register
-      def initialize(regs, align, minimum, syscall_str, stack_register)
-        super(regs, align, minimum)
+      def initialize(regs, align, minimum, stack_register, syscall_str)
+        super(regs, align, minimum, stack_register)
         @syscall_str = syscall_str
-        @stack_register = stack_register
       end
     end
 
-    LINUX_I386 = ABI.new([], 4, 0, cdq_pair: %w(eax edx))
-    LINUX_AMD64 = ABI.new(%w(rdi rsi rdx rcx r8 r9), 8, 0, cdq_pair: %w(rax rdx))
+    LINUX_I386 = ABI.new([], 4, 0, 'esp', cdq_pair: %w(eax edx))
+    LINUX_AMD64 = ABI.new(%w(rdi rsi rdx rcx r8 r9), 8, 0, 'rsp', cdq_pair: %w(rax rdx))
 
-    LINUX_I386_SYSCALL = SyscallABI.new(%w(eax ebx ecx edx esi edi ebp), 4, 0, 'int 0x80', 'esp')
-    LINUX_AMD64_SYSCALL = SyscallABI.new(%w(rax rdi rsi rdx r10 r8 r9), 8, 0, 'syscall', 'rsp')
+    LINUX_I386_SYSCALL = SyscallABI.new(%w(eax ebx ecx edx esi edi ebp), 4, 0, 'esp', 'int 0x80')
+    LINUX_AMD64_SYSCALL = SyscallABI.new(%w(rax rdi rsi rdx r10 r8 r9), 8, 0, 'rsp', 'syscall')
   end
 end
