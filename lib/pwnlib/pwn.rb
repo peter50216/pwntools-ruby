5af98240d670084103a3f8eba68fda633e564350
Peter Shih <peter50216@gmail.com>
2017-04-12 05:26:17 -0700

    spaghetti. DO NOT MERGE!

--- lib/pwnlib/pwn.rb
+++ lib/pwnlib/pwn.rb
@@ -7,6 +7,7 @@ require 'pwnlib/constants/constants'
 require 'pwnlib/context'
 require 'pwnlib/dynelf'
 require 'pwnlib/reg_sort'
+require 'pwnlib/tubes/sock'
 
 require 'pwnlib/util/cyclic'
 require 'pwnlib/util/fiddling'
