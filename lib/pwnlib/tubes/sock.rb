4711571b65220f4b21f53cad0c53bf51781c6444
hanhanW <hanhan0912@gmail.com>
2017-08-10 14:09:10 +0800

    Handle connection error

--- lib/pwnlib/tubes/sock.rb
+++ lib/pwnlib/tubes/sock.rb
@@ -8,11 +8,19 @@ module Pwnlib
   module Tubes
     # Socket!
     class Sock < Tube
+      # Instantiate an {Pwnlib::Tubes::Sock} object.
+      #
+      # @param [String] host
+      #   The host to connect.
+      #
+      # @param [Integer] port
+      #   The port to connect.
       def initialize(host, port)
         super()
         @sock = TCPSocket.new(host, port)
         @sock.binmode
         @timeout = :forever
+        @closed = { recv: false, send: false }
       end
 
       def timeout_raw=(timeout)
@@ -20,21 +28,41 @@ module Pwnlib
       end
 
       def send_raw(data)
-        @sock.write(data)
+        raise EOFError if @closed[:send]
+        begin
+          @sock.write(data)
+        rescue Errno::EPIPE, Errno::ECONNRESET, Errno::ECONNREFUSED
+          shutdown(:send)
+          raise EOFError
+        end
       end
 
       def recv_raw(size)
-        rs, = IO.select([@sock], [], [], @timeout)
-        return if rs.nil?
-        @sock.readpartial(size)
+        raise EOFError if @closed[:recv]
+        loop do
+          begin
+            rs, = IO.select([@sock], [], [], @timeout)
+            return if rs.nil?
+            @sock.readpartial(size)
+          rescue Errno::EAGAIN
+            ''
+          rescue Errno::ECONNREFUSED, Errno::ECONNRESET
+            shutdown(:recv)
+            raise EOFError
+          rescue Errno::EINTR
+            next
+          end
+        end
       end
 
       def io
         @sock
       end
 
-      def io
-        @sock
+      private
+
+      def shutdown(direction)
+        @closed[direction] = true
       end
     end
   end
