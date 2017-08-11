35a384722deec3dfa3f80b44ffa9623d1bcf8862
hanhanW <hanhan0912@gmail.com>
2017-08-12 00:31:07 +0800

    Fix timer test

--- test/timer_test.rb
+++ test/timer_test.rb
@@ -7,19 +7,15 @@ require 'pwnlib/timer'
 class TimerTest < MiniTest::Test
   include ::Pwnlib
 
-  def test_basic
+  def test_countdown
     t = Timer.new
-    class << t
-      attr_writer :deadline
-    end
-
     assert_nil(t.started?)
     assert_nil(t.active?)
-    t.timeout = nil
-    t.deadline = :forever
-    exception = assert_raises(RuntimeError) { t.timeout = nil }
-    assert_equal("Can't change timeout when countdown", exception.message)
-    exception = assert_raises(RuntimeError) { t.countdown(0.1) {} }
+    assert_equal('DARKHH QQ', t.countdown(0.1) { 'DARKHH QQ' })
+    exception = assert_raises(RuntimeError) { t.countdown(0.1) { t.countdown(0.1) {} } }
     assert_equal('Nested countdown not permitted', exception.message)
+    t.timeout = 0.514
+    exception = assert_raises(RuntimeError) { t.countdown(0.1) { t.timeout = :forever } }
+    assert_equal("Can't change timeout when countdown", exception.message)
   end
 end
