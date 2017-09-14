0777710c68df956e6dcfaf696dc99438bfd5ddfb
david942j <david942j@gmail.com>
2017-09-15 00:22:40 +0800

    Util::Getendent

---
+++ lib/pwnlib/util/getendent.rb
@@ -0,0 +1,80 @@
+# encoding: ASCII-8BIT
+
+require 'bindata'
+
+require 'pwnlib/context'
+
+module Pwnlib
+  module Util
+    # Helper methods related to getendent syscall.
+    module Getendent
+      DT_TYPE_REVERSE = {
+        0 => 'UNKNOWN',
+        1 => 'FIFO',
+        2 => 'CHR',
+        4 => 'DIR',
+        6 => 'BLK',
+        8 => 'REG',
+        10 => 'LNK',
+        12 => 'SOCK'
+      }.freeze
+      # The +linux_dirent+ structure.
+      class Dirent < ::BinData::Record
+        attr_accessor :bits
+        # struct linux_dirent {
+        #   unsigned long  d_ino;     /* Inode number */
+        #   unsigned long  d_off;     /* Offset to next linux_dirent */
+        #   unsigned short d_reclen;  /* Length of this linux_dirent */
+        #   char           d_name[];  /* Filename (null-terminated) */
+        #                     /* length is actually (d_reclen - 2 -
+        #                        offsetof(struct linux_dirent, d_name)) */
+        #   /*
+        #   char           pad;       // Zero padding byte
+        #   char           d_type;    // File type (only since Linux
+        #                             // 2.6.4); offset is (d_reclen - 1)
+        #   */
+        # }
+        endian :big_and_little
+        choice :d_ino, selection: :bits, choices: { 32 => :uint32, 64 => :uint64 }
+        choice :d_off, selection: :bits, choices: { 32 => :uint32, 64 => :uint64 }
+        uint16 :d_reclen
+        string :d_name, read_length: -> { d_reclen - d_ino.num_bytes - d_off.num_bytes - 4 }
+        int8 :pad
+        int8 :d_type
+      end
+
+      module_function
+
+      # Parse the output of getendent.
+      # For users to handle the shit-like output by +shellcraft.ls+.
+      #
+      # @param [String] binstr
+      #   The content returns by getendent syscall.
+      #
+      # @return [String]
+      #   Formatted output of filenames with file types.
+      #
+      # @example
+      #   context.arch = 'i386'
+      #   Util::Getendent.parse("\x92\x22\x0e\x01\x8f\x4a\xb3\x41" \
+      #                         "\x18\x00\x52\x45\x41\x44\x4d\x45" \
+      #                         "\x2e\x6d\x64\x00\x00\x00\x00\x08" \
+      #                         "\xb5\x10\x34\x01\xff\xff\xff\x7f" \
+      #                         "\x10\x00\x6c\x69\x62\x00\x00\x04")
+      #   #=> "REG README.md\nDIR lib\n"
+      def parse(binstr)
+        str = StringIO.new(binstr)
+        result = StringIO.new
+        until str.eof?
+          ent = Dirent.new(endian: context.endian.to_sym)
+          ent.bits = context.bits
+          ent.read(str)
+          result.puts(DT_TYPE_REVERSE[ent.d_type] + ' ' + ent.d_name.gsub(/\x00.*/, ''))
+        end
+        result.string
+      end
+
+      include ::Pwnlib::Context
+    end
+  end
+end
---
+++ test/util/getendent_test.rb
@@ -0,0 +1,32 @@
+# encoding: ASCII-8BIT
+
+require 'test_helper'
+
+require 'pwnlib/context'
+require 'pwnlib/util/getendent'
+
+class GetendentTest < MiniTest::Test
+  include ::Pwnlib::Context
+  include ::Pwnlib::Util::Getendent
+
+  def test_parse
+    context.local(arch: 'i386') do
+      assert_equal("REG README.md\nDIR lib\n",
+                   parse("\x92\x22\x0e\x01\x8f\x4a\xb3\x41" \
+                         "\x18\x00\x52\x45\x41\x44\x4d\x45" \
+                         "\x2e\x6d\x64\x00\x30\x00\x00\x08" \
+                         "\xb5\x10\x34\x01\xff\xff\xff\x7f" \
+                         "\x10\x00\x6c\x69\x62\x00\x00\x04"))
+    end
+    context.local(arch: 'amd64') do
+      assert_equal("REG README.md\nDIR lib\n",
+                   parse("\x92\x22\x0e\x01\x00\x00\x00\x00" \
+                         "\x3d\xf6\x7c\x45\x8f\x4a\xb3\x41" \
+                         "\x20\x00\x52\x45\x41\x44\x4d\x45" \
+                         "\x2e\x6d\x64\x00\x30\x00\x00\x08" \
+                         "\xb5\x10\x34\x01\x00\x00\x00\x00" \
+                         "\xff\xff\xff\xff\xff\xff\xff\x7f" \
+                         "\x18\x00\x6c\x69\x62\x00\x00\x04"))
+    end
+  end
+end
