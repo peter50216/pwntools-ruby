8d8d8052b6fbfa87a27b5ccc1ade649d25f050d2
david942j <david942j@gmail.com>
2018-01-25 21:37:58 +0800

    fix #58

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,5 +20,25 @@ module MiniTest
       # Default to disable coloring for easier testing.
       Rainbow.enabled = false
     end
+
+    # Some methods for hooking logger,
+    # require 'pwnlib/logger' before using these methods.
+
+    def log_null(&block)
+      # Windows not support File.open('/dev/null')
+      log_hook(StringIO.new, &block)
+    end
+
+    def log_stdout(&block)
+      log_hook($stdout, &block)
+    end
+
+    def log_hook(obj)
+      old = ::Pwnlib::Logger.log.instance_variable_get(:@logdev)
+      ::Pwnlib::Logger.log.instance_variable_set(:@logdev, obj)
+      res = yield
+      ::Pwnlib::Logger.log.instance_variable_set(:@logdev, old)
+      res
+    end
   end
 end
