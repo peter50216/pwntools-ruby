ad30289271d42a4586715c51ae3a55b521eee182
JonathanBeverley <jonathan.beverley@gmail.com>
2018-05-15 15:43:53 -0400

    serialtube function comments

--- lib/pwnlib/tubes/serialtube.rb
+++ lib/pwnlib/tubes/serialtube.rb
@@ -8,6 +8,18 @@ module Pwnlib
   module Tubes
     # Serial Connections
     class SerialTube < Tube
+      # Instantiate a {Pwnlib::Tubes::SerialTube} object.
+      #
+      # @param [String] port
+      #   A device name for rubyserial to open, e.g. /dev/ttypUSB0
+      # @param [Integer] baudrate
+      #   Baud rate.
+      # @param [Boolean] convert_newlines
+      #   If +true+, will convert local +context.newline+ to +"\\r\\n"+ for remote
+      # @param [Integer] bytesize
+      #   Serial character byte size. The '8' in '8N1'.
+      # @param [Symbol] parity
+      #   Serial character parity. The 'N' in '8N1'.
       def initialize(port = nil, baudrate = 115_200,
                      convert_newlines = true,
                      bytesize = 8, parity = :none)
@@ -21,15 +33,25 @@ module Pwnlib
         @conn = Serial.new(port, baudrate, bytesize, parity)
       end
 
+      # Closes the active connection
       def close
         @conn.close if @conn
         @conn = nil
       end
 
-      # Implementation of the methods required for tube
-
+      # Gets bytes over the serial connection until some bytes are received, or +@timeout+ has passed. It is an error for it to return no data in less than +@timeout+ seconds. It is ok for it to return some data in less time.
+      #
       # Non-blocking, will happily return less than numbytes
       # if that's all that's available
+      # TODO: above is a bug, API for recv_raw requires it be blocking
+      #
+      # @param [Integer] numbytes
+      #   An upper limit on the number of bytes to get.
+      #
+      # @return [String]
+      #   A string containing read bytes.
+      #
+      # @!macro raise_eof
       def recv_raw(numbytes)
         raise EOFError if @conn.nil?
 
@@ -41,10 +63,19 @@ module Pwnlib
         end
       end
 
-      # Writes data. Returns number of bytes written.
+      # Sends bytes over the serial connection. This call will block until all the bytes are sent or an error occurs.
+      #
+      # @param [String] data
+      #   A string of the bytes to send.
+      #
+      # @return [Integer]
+      #   The number of bytes successfully written.
+      #
+      # @!macro raise_eof
       def send_raw(data)
         raise EOFError if @conn.nil?
 
+        # TODO: use context.newline
         data.gsub!(/\n/, "\r\n") if @convert_newlines
         begin
           return @conn.write(data)
@@ -54,6 +85,9 @@ module Pwnlib
         end
       end
 
+      # Sets the +timeout+ to use for subsequent +recv_raw+ calls.
+      #
+      # @param [Integer] timeout
       def timeout_raw=(timeout)
         # XXX: We can't do it, just ignoring
         # In particular, rubyserial doesn't do timeouts, it always returns
@@ -61,17 +95,23 @@ module Pwnlib
         # which covers our needs.
       end
 
+      # Checks to see if the serial connection is active.
+      #
+      # @return [Boolean]
+      #   +true+ iff the connection has been created and not closed.
       def connected_raw
         return false if @conn.nil?
         return false if @conn.closed?
         true
       end
 
+      # Not Implemented.
       def fileno
         raise 'A closed serialtube does not have a file number' if @conn.closed?
         raise 'Not Implemented by rubyserial'
       end
 
+      # Closes the active connection.
       def shutdown_raw
         close
       end
