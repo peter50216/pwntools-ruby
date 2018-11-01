93724dd42a45b1b10ff4f949e248cc16d4179257
david942j <david942j@gmail.com>
2018-11-01 14:34:59 +0800

    Feature: import one-gadget (#131)
    * Implemented ELF#one_gadgets, hence added dependency: one_gadget

--- lib/pwnlib/elf/elf.rb
+++ lib/pwnlib/elf/elf.rb
@@ -1,6 +1,7 @@
 require 'ostruct'
 
 require 'elftools'
+require 'one_gadget/one_gadget'
 require 'rainbow'
 
 require 'pwnlib/logger'
@@ -40,14 +41,15 @@ module Pwnlib
       #   # PIE:      PIE enabled
       #   #=> #<Pwnlib::ELF::ELF:0x00559bd670dcb8>
       def initialize(path, checksec: true)
-        path = File.realpath(path)
+        @path = File.realpath(path)
         @elf_file = ELFTools::ELFFile.new(File.open(path, 'rb'))
         load_got
         load_plt
         load_symbols
         @address = base_address
         @load_addr = @address
-        show_info(path) if checksec
+        @one_gadgets = nil
+        show_info(@path) if checksec
       end
 
       # Set the base address.
@@ -56,6 +58,7 @@ module Pwnlib
       #   got
       #   plt
       #   symbols
+      #   one_gadgets
       #
       # @param [Integer] val
       #   Address to be changed to.
@@ -68,6 +71,7 @@ module Pwnlib
         [@got, @plt, @symbols].compact.each do |tbl|
           tbl.each_pair { |k, _| tbl[k] += val - old }
         end
+        @one_gadgets.map! { |off| off + val - old } if @one_gadgets
       end
 
       # Return the protection information, wrapper with color codes.
@@ -180,6 +184,58 @@ module Pwnlib
         true
       end
       alias find search
+
+      # Returns one-gadgets of glibc.
+      #
+      # @return [Array<Integer>]
+      #   Returns array of one-gadgets, see examples.
+      #
+      # @example
+      #   ELF::ELF.new('/lib/x86_64-linux-gnu/libc.so.6').one_gadgets[0]
+      #   #=> 324293 # 0x4f2c5
+      #
+      # @example
+      #   libc = ELF::ELF.new('/lib/x86_64-linux-gnu/libc.so.6')
+      #   libc.one_gadgets[1]
+      #   #=> 324386 # 0x4f322
+      #
+      #   libc.address = 0x7fff7fff0000
+      #   libc.one_gadgets[1]
+      #   #=> 140735341130530 # 0x7fff8003f322
+      #
+      # @example
+      #   libc = ELF::ELF.new('/lib/x86_64-linux-gnu/libc.so.6')
+      #   context.log_level = :debug
+      #   libc.one_gadgets[0]
+      #   # [DEBUG] 0x4f2c5 execve("/bin/sh", rsp+0x40, environ)
+      #   # constraints:
+      #   #   rcx == NULL
+      #   #=> 324293
+      def one_gadgets
+        return @one_gadgets if @one_gadgets
+
+        gadgets = OneGadget.gadgets(file: @path, details: true, level: 1)
+        @one_gadgets = gadgets.map { |g| g.offset + address }
+        @one_gadgets.instance_variable_set(:@gadgets, gadgets)
+
+        class << @one_gadgets
+          def [](idx)
+            super.tap { log.debug(@gadgets[idx].inspect) }
+          end
+
+          def first
+            self[0]
+          end
+
+          def last
+            self[-1]
+          end
+
+          include ::Pwnlib::Logger
+        end
+
+        @one_gadgets
+      end
 
       private
 
