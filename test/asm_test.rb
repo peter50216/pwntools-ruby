fcde6336e6d482a6ff4469aad359acf755e26972
david942j <david942j@gmail.com>
2019-02-19 05:13:05 -0800

    Change number of spaces to have better alignement

--- test/asm_test.rb
+++ test/asm_test.rb
@@ -26,7 +26,7 @@ class AsmTest < MiniTest::Test
       # fetch `<vma>:`
       vma = test.scan(/^\s*(\w+):/).flatten.first.to_i(16)
       # fetch bytes
-      bytes = [test.scan(/^\s*\w+:\s*(([\da-f]{2}\s)+)\s*[a-z]/).map(&:first).join.split.join].pack('H*')
+      bytes = [test.scan(/^\s*\w+:\s{3}(([\da-f]{2}\s)+)\s/).map(&:first).join.split.join].pack('H*')
       output = test
       output << "\n" unless output.end_with?("\n")
       yield(bytes, vma, test, **metadata)
@@ -62,24 +62,24 @@ class AsmTest < MiniTest::Test
       str = Asm.disasm("h\x01\x01\x01\x01\x814$ri\x01\x011\xd2"\
                        "Rj\x04Z\x01\xe2R\x89\xe2jhh///sh/binj\x0bX\x89\xe3\x89\xd1\x99\xcd\x80")
       assert_equal(<<-EOS, str)
-   0:   68 01 01 01 01       push    0x1010101
-   5:   81 34 24 72 69 01 01 xor     dword ptr [esp], 0x1016972
-   c:   31 d2                xor     edx, edx
-   e:   52                   push    edx
-   f:   6a 04                push    4
-  11:   5a                   pop     edx
-  12:   01 e2                add     edx, esp
-  14:   52                   push    edx
-  15:   89 e2                mov     edx, esp
-  17:   6a 68                push    0x68
-  19:   68 2f 2f 2f 73       push    0x732f2f2f
-  1e:   68 2f 62 69 6e       push    0x6e69622f
-  23:   6a 0b                push    0xb
-  25:   58                   pop     eax
-  26:   89 e3                mov     ebx, esp
-  28:   89 d1                mov     ecx, edx
-  2a:   99                   cdq
-  2b:   cd 80                int     0x80
+   0:   68 01 01 01 01        push 0x1010101
+   5:   81 34 24 72 69 01 01  xor  dword ptr [esp], 0x1016972
+   c:   31 d2                 xor  edx, edx
+   e:   52                    push edx
+   f:   6a 04                 push 4
+  11:   5a                    pop  edx
+  12:   01 e2                 add  edx, esp
+  14:   52                    push edx
+  15:   89 e2                 mov  edx, esp
+  17:   6a 68                 push 0x68
+  19:   68 2f 2f 2f 73        push 0x732f2f2f
+  1e:   68 2f 62 69 6e        push 0x6e69622f
+  23:   6a 0b                 push 0xb
+  25:   58                    pop  eax
+  26:   89 e3                 mov  ebx, esp
+  28:   89 d1                 mov  ecx, edx
+  2a:   99                    cdq
+  2b:   cd 80                 int  0x80
       EOS
     end
   end
@@ -91,32 +91,32 @@ class AsmTest < MiniTest::Test
                        "Rj\x08ZH\x01\xe2RH\x89\xe2jhH\xb8/bin///sPj;XH\x89\xe7H\x89\xd6\x99\x0f\x05", vma: 0xfff)
 
       assert_equal(<<-EOS, str)
-   fff:   68 72 69 01 01                push    0x1016972
-  1004:   81 34 24 01 01 01 01          xor     dword ptr [rsp], 0x1010101
-  100b:   31 d2                         xor     edx, edx
-  100d:   52                            push    rdx
-  100e:   6a 08                         push    8
-  1010:   5a                            pop     rdx
-  1011:   48 01 e2                      add     rdx, rsp
-  1014:   52                            push    rdx
-  1015:   48 89 e2                      mov     rdx, rsp
-  1018:   6a 68                         push    0x68
-  101a:   48 b8 2f 62 69 6e 2f 2f 2f 73 movabs  rax, 0x732f2f2f6e69622f
-  1024:   50                            push    rax
-  1025:   6a 3b                         push    0x3b
-  1027:   58                            pop     rax
-  1028:   48 89 e7                      mov     rdi, rsp
-  102b:   48 89 d6                      mov     rsi, rdx
-  102e:   99                            cdq
-  102f:   0f 05                         syscall
+   fff:   68 72 69 01 01                 push    0x1016972
+  1004:   81 34 24 01 01 01 01           xor     dword ptr [rsp], 0x1010101
+  100b:   31 d2                          xor     edx, edx
+  100d:   52                             push    rdx
+  100e:   6a 08                          push    8
+  1010:   5a                             pop     rdx
+  1011:   48 01 e2                       add     rdx, rsp
+  1014:   52                             push    rdx
+  1015:   48 89 e2                       mov     rdx, rsp
+  1018:   6a 68                          push    0x68
+  101a:   48 b8 2f 62 69 6e 2f 2f 2f 73  movabs  rax, 0x732f2f2f6e69622f
+  1024:   50                             push    rax
+  1025:   6a 3b                          push    0x3b
+  1027:   58                             pop     rax
+  1028:   48 89 e7                       mov     rdi, rsp
+  102b:   48 89 d6                       mov     rsi, rdx
+  102e:   99                             cdq
+  102f:   0f 05                          syscall
       EOS
     end
   end
 
-  def test_disasm_all
-    skip_windows
-
-    Dir.glob(File.join(__dir__, 'data', 'assembly', '*.s')) do |file|
+  # Defining methods dynamically makes proper error message be shown when tests failed.
+  Dir.glob(File.join(__dir__, 'data', 'assembly', '*.s')) do |file|
+    __send__(:define_method, "test_disasm_file_#{File.basename(file, '.s')}") do
+      skip_windows
       count = 0
       parse_sfile(file) do |bytes, vma, output, **ctx|
         context.local(**ctx) do
--- test/data/assembly/i386.s
+++ test/data/assembly/i386.s
@@ -1,22 +1,22 @@
 # arch: i386
 # simple move
-  0:   b8 5d 00 00 00 mov     eax, 0x5d
+  0:   b8 5d 00 00 00  mov eax, 0x5d
 
 # pwntools-python's shellcraft.sh()
-   0:   6a 68                push    0x68
-   2:   68 2f 2f 2f 73       push    0x732f2f2f
-   7:   68 2f 62 69 6e       push    0x6e69622f
-   c:   89 e3                mov     ebx, esp
-   e:   68 01 01 01 01       push    0x1010101
-  13:   81 34 24 72 69 01 01 xor     dword ptr [esp], 0x1016972
-  1a:   31 c9                xor     ecx, ecx
-  1c:   51                   push    ecx
-  1d:   6a 04                push    4
-  1f:   59                   pop     ecx
-  20:   01 e1                add     ecx, esp
-  22:   51                   push    ecx
-  23:   89 e1                mov     ecx, esp
-  25:   31 d2                xor     edx, edx
-  27:   6a 0b                push    0xb
-  29:   58                   pop     eax
-  2a:   cd 80                int     0x80
+   0:   6a 68                 push 0x68
+   2:   68 2f 2f 2f 73        push 0x732f2f2f
+   7:   68 2f 62 69 6e        push 0x6e69622f
+   c:   89 e3                 mov  ebx, esp
+   e:   68 01 01 01 01        push 0x1010101
+  13:   81 34 24 72 69 01 01  xor  dword ptr [esp], 0x1016972
+  1a:   31 c9                 xor  ecx, ecx
+  1c:   51                    push ecx
+  1d:   6a 04                 push 4
+  1f:   59                    pop  ecx
+  20:   01 e1                 add  ecx, esp
+  22:   51                    push ecx
+  23:   89 e1                 mov  ecx, esp
+  25:   31 d2                 xor  edx, edx
+  27:   6a 0b                 push 0xb
+  29:   58                    pop  eax
+  2a:   cd 80                 int  0x80
