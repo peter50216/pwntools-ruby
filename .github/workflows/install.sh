5c6a5195e889f9e0d8936dfa84f4e8207ef9b288
david942j <david942j@gmail.com>
2021-01-19 01:21:23 +0800

    Update install.sh

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,27 +1,11 @@
 set -e -x
-local_deb_extract()
-{
-  wget $1
-  ar vx *.deb
-  tar xvf data.tar.*
-  rm -f *.tar.* *deb*
-}
-
-install_deb()
-{
-  package=$1
-  echo "Installing $package"
-  INDEX="https://packages.ubuntu.com/en/bionic/amd64/$package/download"
-  URL=$(curl "$INDEX" | grep -Eo "https?://.*$package.*\.deb" | head -1)
-  local_deb_extract "$URL"
-}
 
 install_keystone_from_source()
 {
-  # keystone can only build from source
+  # keystone can only be built from source
   # https://github.com/keystone-engine/keystone/blob/master/docs/COMPILE-NIX.md
   #
-  # XXX(david942j): How to prevent compile every time on Travis-CI?
+  # XXX(david942j): How to prevent it from being compiled every time?
   git clone https://github.com/keystone-engine/keystone.git
   # rvm does lots of things on OSX when cwd changing.. use bash without rvm to prevent this.
   /bin/bash --norc -c 'mkdir keystone/build && cd keystone/build && ../make-share.sh'
@@ -30,9 +14,7 @@ install_keystone_from_source()
 setup_linux()
 {
   sudo apt update
-  sudo apt install --force-yes gcc-multilib g++-multilib binutils socat
-  # install capstone
-  sudo apt install libcapstone3
+  sudo apt install --force-yes gcc-multilib g++-multilib binutils socat libcapstone3
 
   # install keystone
   install_keystone_from_source
