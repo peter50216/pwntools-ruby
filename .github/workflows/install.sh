3028f306059413c4b4531d0b3950ca82122e4ff6
david942j <david942j@gmail.com>
2021-01-18 23:34:43 +0800

    Add GitHub action

---
+++ .github/workflows/install.sh
@@ -0,0 +1,61 @@
+set -e -x
+local_deb_extract()
+{
+  wget $1
+  ar vx *.deb
+  tar xvf data.tar.*
+  rm -f *.tar.* *deb*
+}
+
+install_deb()
+{
+  package=$1
+  echo "Installing $package"
+  INDEX="https://packages.ubuntu.com/en/bionic/amd64/$package/download"
+  URL=$(curl "$INDEX" | grep -Eo "https?://.*$package.*\.deb" | head -1)
+  local_deb_extract "$URL"
+}
+
+install_keystone_from_source()
+{
+  # keystone can only build from source
+  # https://github.com/keystone-engine/keystone/blob/master/docs/COMPILE-NIX.md
+  #
+  # XXX(david942j): How to prevent compile every time on Travis-CI?
+  git clone https://github.com/keystone-engine/keystone.git
+  # rvm does lots of things on OSX when cwd changing.. use bash without rvm to prevent this.
+  /bin/bash --norc -c 'mkdir keystone/build && cd keystone/build && ../make-share.sh'
+}
+
+setup_linux()
+{
+  sudo apt update
+  sudo apt install --force-yes gcc-multilib g++-multilib binutils socat
+  # install capstone
+  sudo apt install libcapstone3
+
+  # install keystone
+  install_keystone_from_source
+}
+
+setup_osx()
+{
+  # install capstone
+  brew install capstone
+
+  # install keystone
+  install_keystone_from_source
+  # hack, don't know why set DYLD_LIBRARY_PATH has no effect
+  # ln -s keystone/build/llvm/lib/libkeystone.dylib libkeystone.dylib
+
+  # install socat
+  brew install socat
+}
+
+if [[ "$1" == "macOS" ]]; then
+  setup_osx
+elif [[ "$1" == "Linux" ]]; then
+  setup_linux
+fi
+
+set +e +x
